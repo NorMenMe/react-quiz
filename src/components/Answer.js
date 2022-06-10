ed05bc8f224664e0eee5ddbf84d55c12e17e127a
nmetz <info@normanmetz.com>
2022-06-10 08:25:58 +0200

    wip(main): add logic, if all correct answers are checked increase score

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -17,16 +17,22 @@ let getAnswer = (e) => {
             item.classList.add("answer__item--border-false");
           }
     })
-console.log(solutions[counter]);
+console.log(solutions[counter].length);
     // check enabled checkbox
     let checkboxes = document.querySelectorAll(".answer__checkbox");
+    const rightAnswersArr = [];
     checkboxes.forEach((checkbox,index) => {
       if (checkbox.checked) {
         if (solutions[counter].includes(index)) {
-          setScore(score + 1);
+          rightAnswersArr.push(1);
         }
       }
     })
+    
+    if (solutions[counter].length === rightAnswersArr.length) {
+      setScore(score + 1);
+    }  
+
 
     // show Next button
     setIsSubmit(true);
